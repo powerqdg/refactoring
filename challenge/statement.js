945020842698d3531ddb9a28cad67fb6acd6a5c0
dgsin <powerqdg@gmail.com>
2024-01-21 22:03:00 +0900

    add test html

--- challenge/statement.js
+++ challenge/statement.js
@@ -23,11 +23,11 @@ function renderPlainText(data) {
 function renderHTML(data) {
   let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
   result += '<table>\n';
-  result += '<tr><th>play</th><th>cost</th><th>seats</th></tr>';
+  result += '<tr><th>play</th><th>석</th><th>cost</th></tr>';
   for (let performance of data.performances) {
-    result += `<tr><td>${performance.play.name}</td>`;
-    result += `<td>${usd(performance.amount / 100)}</td>`;
-    result += `<td>${performance.audience}석</td></tr>\n`;
+    result += `  <tr><td>${performance.play.name}</td>`;
+    result += `<td>${performance.audience}</td>`;
+    result += `<td>${usd(performance.amount / 100)}</td></tr>\n`;
   }
   result += '</table>\n';
   result += `<p>총액: <em>${usd(data.totalAmount / 100)}</em></p>\n`;
@@ -72,7 +72,7 @@ const invoicesJSON = [
 
 const plainText = statement(invoicesJSON[0], playsJSON);
 const html = htmlStatement(invoicesJSON[0], playsJSON);
-const expected =
+const expectedText =
   '청구 내역 (고객명: BigCo)\n' +
   '  Hamlet: $650.00 (55석)\n' +
   '  As You Like It: $580.00 (35석)\n' +
@@ -80,9 +80,20 @@ const expected =
   '총액: $1,730.00\n' +
   '적립 포인트: 47점\n';
 
-console.log('============ expected ============');
-console.log(expected);
-console.log('============ plainText ============');
+const expectedHtml =
+  `<h1>청구 내역 (고객명: BigCo)</h1>\n` +
+  `<table>\n` +
+  `<tr><th>play</th><th>석</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n` +
+  `  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n` +
+  `  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n` +
+  `</table>\n` +
+  `<p>총액: <em>$1,730.00</em></p>\n` +
+  `<p>적립 포인트: <em>47</em>점</p>\n`;
+
+console.log(expectedText);
 console.log(plainText);
-console.log('============ html ============');
+console.log(expectedText === plainText);
+console.log('========================');
+console.log(expectedHtml);
 console.log(html);
+console.log(expectedHtml === html);
--- challenge/tests/statement.test.js
+++ challenge/tests/statement.test.js
@@ -1,4 +1,5 @@
 import { statement } from '../statement.js';
+import { htmlStatement } from '../statement.js';
 
 describe('statement', () => {
   const playsJSON = {
@@ -36,6 +37,20 @@ describe('statement', () => {
       '적립 포인트: 47점\n';
 
     expect(statement(invoicesJSON[0], playsJSON)).toBe(expected);
+  });
+
+  it('should print a statement in HTML format', () => {
+    const expected =
+      `<h1>청구 내역 (고객명: BigCo)</h1>\n` +
+      `<table>\n` +
+      `<tr><th>play</th><th>석</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n` +
+      `  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n` +
+      `  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n` +
+      `</table>\n` +
+      `<p>총액: <em>$1,730.00</em></p>\n` +
+      `<p>적립 포인트: <em>47</em>점</p>\n`;
+
+    expect(htmlStatement(invoicesJSON[0], playsJSON)).toBe(expected);
   });
 });
 
