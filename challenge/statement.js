dcec9a82be9364f3b3ec9466958de5e8ceb2cb94
dgsin <powerqdg@gmail.com>
2024-01-21 15:32:26 +0900

    change format function name to usd

--- challenge/statement.js
+++ challenge/statement.js
@@ -11,12 +11,12 @@ export function statement(invoice, plays) {
       volumeCredits += Math.floor(performance.audience / 5);
 
     // 청구 내역을 출력한다.
-    result += `  ${playFor(performance).name}: ${format(
+    result += `  ${playFor(performance).name}: ${usd(
       amountFor(performance) / 100
     )} (${performance.audience}석)\n`;
     totalAmount += amountFor(performance);
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 
@@ -47,7 +47,7 @@ export function statement(invoice, plays) {
   }
 }
 
-function format(number) {
+function usd(number) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
