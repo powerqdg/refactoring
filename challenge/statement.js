2c5b7e0fa0d6360a3b984b2b81322dcc0df32887
dgsin <powerqdg@gmail.com>
2024-01-21 15:09:14 +0900

    extract amountFor function

--- challenge/statement.js
+++ challenge/statement.js
@@ -3,41 +3,46 @@ export function statement(invoice, plays) {
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case 'tragedy': // 비극
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
+  for (let performance of invoice.performances) {
+    const play = plays[performance.playID];
+    let thisAmount = amountFor(play, performance);
 
     // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(performance.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === play.type)
+      volumeCredits += Math.floor(performance.audience / 5);
 
     // 청구 내역을 출력한다.
     result += `  ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
+      performance.audience
     }석)\n`;
     totalAmount += thisAmount;
   }
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+}
+
+function amountFor(play, performance) {
+  let result = 0;
+  switch (play.type) {
+    case 'tragedy': // 비극
+      result = 40000;
+      if (performance.audience > 30) {
+        result += 1000 * (performance.audience - 30);
+      }
+      break;
+    case 'comedy': // 희극
+      result = 30000;
+      if (performance.audience > 20) {
+        result += 10000 + 500 * (performance.audience - 20);
+      }
+      result += 300 * performance.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
   return result;
 }
 
